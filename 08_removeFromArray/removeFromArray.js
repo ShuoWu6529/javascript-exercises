72a56dc8a941d5f2578feed849205ac21bc89fed
Shuo <186079699+ShuoWu6529@users.noreply.github.com>
2025-07-08 14:39:37 -0400

    Complete problem 6 to 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...otherArguments) {
+    let result = [];
+    for (const element of arr) {
+        if (otherArguments.includes(element)){
+            continue;
+        }
+        result.push(element);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
 
+   if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    let smaller = num1;
+    let larger = num2;
+    if (num1 > num2){
+        smaller = num2;
+        larger = num1;
+    }
+    let result = 0;
+    for (let i = smaller; i <= larger; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
