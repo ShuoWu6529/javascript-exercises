c10ef6ca1a1b4850419f3d0fc8e303562cc65884
Shuo <186079699+ShuoWu6529@users.noreply.github.com>
2025-07-15 01:13:05 -0400

    Complete 12 to 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const match = str.toLowerCase().match(/[a-z0-9]/g).join('');
+    const reverse = match.split('').reverse().join('');
 
+    return match === reverse;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let newPeople = people.sort((a,b) => {
+        let aDeathYear = currentYear;
+        if ("yearOfDeath" in a) {
+            aDeathYear = a.yearOfDeath;
+        }
 
+        let bDeathYear = currentYear;
+        if ("yearOfDeath" in b) {
+            bDeathYear = b.yearOfDeath;
+        }
+
+        return ( (aDeathYear - a.yearOfBirth) - (bDeathYear - b.yearOfBirth) )
+    })
+    return newPeople[people.length-1];
 };
 
 // Do not edit below this line
